80a1949f381fedcfa9288d4f77d25389d0b46576
Roman Kosov <11503318+91k@users.noreply.github.com>
2024-08-28 23:29:22 +0300

    fix: Добавляет плагин копирования, исправляет clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
+const copyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/main.js',
+    devtool: 'source-map',
+    plugins: [
+        new copyWebpackPlugin({
+          patterns: [
+            { from: 'public', to: "." },
+          ],
+        }),
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
+        clean: true,
     },
-    clean: true,
-    devtool: 'source-map',
 };
